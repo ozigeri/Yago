933fb450a8048ca941f56855ced2cd54a0648030
Gerison02 <gergokohler@gmail.com>
2025-11-14 22:53:02 +0100

    feat: define TemplateModel

---
+++ Yago/Templates/TemplateModel/TemplateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yago.Templates.TemplateModel
+{
+    internal class TemplateModel
+    {
+        public string Name { get; set; }
+        public string Php { get; set; }
+        public string Composer { get; set; }
+        public string Node { get; set; }
+
+        public TemplateModel(string name, string php, string composer, string node)
+        {
+            Name = name;
+            Php = php;
+            Composer = composer;
+            Node = node;
+        }
+    }
+}
