9860996ed364441cb633fb7dafc2fc9f49b58734
hunti-ekke <lenartjanos12@gmail.com>
2025-11-23 13:23:53 +0100

    feat: implement directory check and cleanup for Laravel projects

--- Yago/RepositoryCreator/CMD/phpCMDCommand.cs
+++ Yago/RepositoryCreator/CMD/phpCMDCommand.cs
@@ -43,6 +43,27 @@ namespace Yago.RepositoryCreator.CMD
           
             sb.AppendLine("@echo off");
             sb.AppendLine($"cd /d \"{location}\"");
+            sb.AppendLine($"if not exist \"{name}\" goto :START_INSTALL");
+            sb.AppendLine("\tcolor 0C");
+            sb.AppendLine("\techo.");
+            sb.AppendLine($"\techo !!!!!!!!! HIBA: A(Z) {name} MAPPA MAR LETEZIK !!!!!!!!!");
+            sb.AppendLine("\techo Szeretned TOROLNI a meglevo mappat A TELJES TARTALMAVAL es folytatni a telepitest?");
+            sb.AppendLine("\techo.");
+
+            sb.AppendLine("set /p user_answer=Nyomj 'I' betut a torleshez, vagy Entert a kilepeshez: ");
+            sb.AppendLine("if /i \"%user_answer%\" neq \"I\" (");
+            sb.AppendLine("\techo A telepites megszakitva.");
+            sb.AppendLine("\tpause");
+            sb.AppendLine("\texit");
+            sb.AppendLine(")");
+
+            sb.AppendLine("echo.");
+            sb.AppendLine($"echo Mappa torlese: {name}...");
+            sb.AppendLine($"rd /s /q \"{name}\"");
+            sb.AppendLine("color 07");
+            sb.AppendLine("cls");
+
+            sb.AppendLine(":START_INSTALL");
             sb.AppendLine("set PHPRC=");
             sb.AppendLine("set PHP_INI_SCAN_DIR=");
             sb.AppendLine($"set \"PATH={phpPath};%PATH%\"");
@@ -62,7 +83,7 @@ namespace Yago.RepositoryCreator.CMD
             sb.AppendLine("echo.");
             sb.AppendLine($"echo A project elkeszult a(z) {name} mappaban.");
             sb.AppendLine("echo.");
-            sb.AppendLine("timeout /t 50 /nobreak >nul");
+            sb.AppendLine("timeout /t 5 /nobreak >nul");
             sb.AppendLine("exit");
 
 
