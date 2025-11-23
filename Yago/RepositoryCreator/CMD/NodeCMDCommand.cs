7fe7ba52cb7505b249332345e41b6ecad823d95f
hunti-ekke <lenartjanos12@gmail.com>
2025-11-23 13:19:15 +0100

    feat: implement Node.js project creation with Vite and error handling

--- Yago/RepositoryCreator/CMD/NodeCMDCommand.cs
+++ Yago/RepositoryCreator/CMD/NodeCMDCommand.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yago.Versions.Core;
 
 namespace Yago.RepositoryCreator.CMD
 {
@@ -24,18 +25,76 @@ namespace Yago.RepositoryCreator.CMD
         public void Execute(bool opensBrowser)
         {
             string tempBatPath = Path.Combine(Path.GetTempPath(), "node_temp_start.bat");
+            string nodeDir = Path.Combine(EnvironmentManager.BasePaths[Versions.Enums.VersionType.NodeJs], $"v{version}");
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("@echo off");
-            sb.AppendLine($"cd {location}");
-            sb.AppendLine($"set \"NODE_HOME=C:\\nodejs\\v{version}\"");
+            sb.AppendLine($"cd /d \"{location}\"");
+
+            sb.AppendLine($"if not exist \"{name}\" goto :START_INSTALL");
+            sb.AppendLine("\tcolor 0C");
+            sb.AppendLine("\techo.");
+            sb.AppendLine($"\techo !!!!!!!!! HIBA: A(Z) {name} MAPPA MAR LETEZIK !!!!!!!!!");
+            sb.AppendLine("\techo Szeretned TOROLNI a meglevo mappat A TELJES TARTALMAVAL es folytatni a telepitest?");
+            sb.AppendLine("\techo.");
+
+            sb.AppendLine("set /p user_answer=Nyomj 'I' betut a torleshez, vagy Entert a kilepeshez: ");
+            sb.AppendLine("if /i \"%user_answer%\" neq \"I\" (");
+            sb.AppendLine("\techo A telepites megszakitva.");
+            sb.AppendLine("\tpause");
+            sb.AppendLine("\texit");
+            sb.AppendLine(")");
+
+            sb.AppendLine("echo.");
+            sb.AppendLine($"echo Mappa torlese: {name}...");
+            sb.AppendLine($"rd /s /q \"{name}\"");
+            sb.AppendLine("color 07");
+            sb.AppendLine("cls");
+
+            sb.AppendLine(":START_INSTALL");
+            sb.AppendLine($"set \"NODE_HOME={nodeDir}\"");
             sb.AppendLine("set \"PATH=%NODE_HOME%;%PATH%\"");
-            sb.AppendLine($"echo No | npm create vite@latest {name} -- --template {app.ToLower()}\"");
-            sb.AppendLine($"cd {name}");
+            sb.AppendLine("echo.");
+            sb.AppendLine("echo Vite projekt letrehozasa...");
+            sb.AppendLine($"echo No | call npm create vite@latest \"{name}\" -- --template {app.ToLower()}");
+
+            sb.AppendLine("if %errorlevel% neq 0 (");
+            sb.AppendLine("\tcolor 0C");
+            sb.AppendLine("\techo.");
+            sb.AppendLine("\techo !!!!!!!!! HIBA: NEM SIKERULT LETREHOZNI A PROJEKTET !!!!!!!!!");
+            sb.AppendLine("\tpause");
+            sb.AppendLine("\texit");
+            sb.AppendLine(")");
+
+            sb.AppendLine($"cd \"{name}\"");
+            sb.AppendLine("echo.");
+            sb.AppendLine("echo Fuggosegek telepitese (npm install)...");
             sb.AppendLine("call npm install");
-            sb.AppendLine("echo Repo created!");
-            if (opensBrowser) sb.AppendLine("start http://localhost:5173");
-            sb.AppendLine("start cmd /k \"npm run dev\"");
+
+            sb.AppendLine("if %errorlevel% neq 0 (");
+            sb.AppendLine("\tcolor 0C");
+            sb.AppendLine("\techo.");
+            sb.AppendLine("\techo !!!!!!!!! HIBA: A FUGGOSEGEK TELEPITESE SIKERTELEN !!!!!!!!!");
+            sb.AppendLine("\tpause");
+            sb.AppendLine("\texit");
+            sb.AppendLine(")");
+
+            sb.AppendLine("cls");
+            sb.AppendLine("color 0A");
+            sb.AppendLine("echo.");
+            sb.AppendLine("echo Sikeres telepites!");
+            sb.AppendLine($"echo A projekt elkeszult a(z) {name} mappaban.");
+            sb.AppendLine("start \"Vite Dev Server\" cmd /k \"npm run dev\"");
+
+            if (opensBrowser)
+            {
+                sb.AppendLine("timeout /t 5 /nobreak >nul");
+                sb.AppendLine("start http://localhost:5173");
+            }
+            else
+            {
+                sb.AppendLine("timeout /t 5 /nobreak >nul");
+            }
             sb.AppendLine("exit");
 
 
