c9c0f9a188f19705119ff730dbc90fd718d4f4e2
hunti-ekke <lenartjanos12@gmail.com>
2025-12-02 14:27:59 +0100

    feat: create settings form with input validation for github auth

---
+++ Yago/RepositoryCreator/FormGitHubSettings.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Yago.RepositoryCreator
+{
+    partial class FormGitHubSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.tokenBox = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(121, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Felhasználónév";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(124, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GitHub token ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(87, 72);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(177, 20);
+            this.userBox.TabIndex = 2;
+            // 
+            // tokenBox
+            // 
+            this.tokenBox.Location = new System.Drawing.Point(87, 136);
+            this.tokenBox.Name = "tokenBox";
+            this.tokenBox.PasswordChar = '*';
+            this.tokenBox.Size = new System.Drawing.Size(177, 20);
+            this.tokenBox.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 199);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 25);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Mentés";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(189, 199);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 25);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Mégse";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FormGitHubSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 271);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tokenBox);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormGitHubSettings";
+            this.Text = "FormGitHubSettings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox tokenBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Yago/RepositoryCreator/FormGitHubSettings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yago.RepositoryCreator
+{
+    public partial class FormGitHubSettings : Form
+    {
+        public FormGitHubSettings()
+        {
+            InitializeComponent();
+            userBox.Text = Properties.Settings.Default.GitHubUsername;
+            tokenBox.Text = Properties.Settings.Default.GitHubToken;
+        }
+        private bool GHValidation()
+        {
+            if (string.IsNullOrWhiteSpace(userBox.Text) || string.IsNullOrWhiteSpace(tokenBox.Text))
+            {
+                MessageBox.Show("Kérlek töltsd ki mindkét mezőt!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (!tokenBox.Text.StartsWith("ghp_") && !tokenBox.Text.StartsWith("github_pat_"))
+            {
+                var result = MessageBox.Show("A token formátuma nem megfelelő! ('ghp_' vagy 'github_pat_') kezdetű.", "Figyelmeztetés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.No) return false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (GHValidation())
+            {
+                Properties.Settings.Default.GitHubUsername = userBox.Text;
+                Properties.Settings.Default.GitHubToken = tokenBox.Text;
+                Properties.Settings.Default.Save();
+
+                MessageBox.Show("Sikeres mentés!", "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
